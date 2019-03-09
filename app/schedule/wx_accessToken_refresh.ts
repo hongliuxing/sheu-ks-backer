cfaefa218f01e2ecb9cceb2b70a4aadbf677ba0a
Alalabu <253993420@qq.com>
2019-03-10 02:14:29 +0800

    刷新access_token修改

--- app/schedule/wx_accessToken_refresh.ts
+++ app/schedule/wx_accessToken_refresh.ts
@@ -4,7 +4,7 @@
  */
 export const schedule = {
     // interval: '60m',
-    cron: '* * 1 * * *',
+    cron: '* * */1 * * *',
     immediate: true, // 配置了该参数为 true 时，这个定时任务会在应用启动并 ready 后立刻执行一次这个定时任务。
     type: 'worker', // run in all workers
 };
